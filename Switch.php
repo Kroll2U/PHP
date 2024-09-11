1ecf30fe00b25f905ba483feb3f2d31252dc3fa1
Krzysztof Rolka <84820173+Kroll2U@users.noreply.github.com>
2024-09-11 19:59:49 +0200

    Create Switch.php

---
+++ Switch.php
@@ -0,0 +1,23 @@
+<?php
+
+ $grade = "A";
+
+ switch($grade){
+    case "A":
+        echo"Udało ci się";
+        break;
+    case "B":
+        echo"Brawo";
+        break;
+    case "C":
+        echo "W miare";
+        break;
+    case "D":
+        echo"Dupa";
+        break;
+    default:
+        echo"Podałeś złą informację";
+ }
+
+
+?>
