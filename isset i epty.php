925e61773a8be30ef87e9bb596535b438e518ad8
Krzysztof Rolka <84820173+Kroll2U@users.noreply.github.com>
2024-09-15 18:53:04 +0200

    isset-empty.php

---
+++ isset i epty.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+    <label>username:</label>
+    <input type="text" name="username">
+    <label>password:</label>
+    <input type="text" name="password">
+    <input type="submit" name="login" value="Log in">
+    </form>
+</body>
+</html>
+
+
+
+<?php
+
+//isset() = zwraca TRUE gdy zmienna jest zaklerowana i nie wynosi 0
+// empty() = zwraca True jeśli zmienna nie jest zadeklarowana, false ,null
+
+
+    // foreach ($_POST as $key => $value){
+    //     echo "{$key} = {$value}<br>";
+    // }
+
+
+    if(isset($_POST["login"])){
+
+       $username = $_POST["username"];
+       $password = $_POST["password"];
+
+        if(empty($username)){
+            echo"Username is missing";
+       }
+       elseif(empty($password)){
+        echo"password is missing";
+       }
+       else{
+        echo"hello {$username}";
+       }
+    }
+?>
+
+
